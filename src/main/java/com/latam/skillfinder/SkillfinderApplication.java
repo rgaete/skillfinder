32d44b0ef83f406c8546a79147b82e02d4e320a6
Ricardo Gaete Duarte <ricardo.gaete@latam.com>
2017-12-09 14:48:53 -0300

    first commit

---
+++ src/main/java/com/latam/skillfinder/SkillfinderApplication.java
@@ -0,0 +1,12 @@
+package com.latam.skillfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillfinderApplication.class, args);
+	}
+}
